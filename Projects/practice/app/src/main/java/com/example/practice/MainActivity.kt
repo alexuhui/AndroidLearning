491ed4ea2aed3914ace670bff2f8bbcbed8df891
chenyouhui1 <chenyouhui1@joyy.com>
2024-02-04 18:30:45 +0800

    mainactivity

--- Projects/practice/app/src/main/java/com/example/practice/MainActivity.kt
+++ Projects/practice/app/src/main/java/com/example/practice/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
@@ -11,39 +12,57 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.practice.main.FirstActivity
+import com.example.practice.main.SecondActivity
 
 const val TAG = "MainActivity"
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent : Intent = Intent(this, FirstActivity::class.java)
-        this.startActivity(intent)
-//        setContent {
-//            PracticeTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colorScheme.background
-//                ) {
-//                    Greeting("Android");
-//                    AddTestBtn(this);
-//
-//
-//                    basicSyntaxTest();
-//                }
-//            }
-//        }
+        setContentView(R.layout.activity_main)
+        initView()
+
+
+        //        val intent : Intent = Intent(this, FirstActivity::class.java)
+        //        this.startActivity(intent)
+        //        setContent {
+        //            PracticeTheme {
+        //                // A surface container using the 'background' color from the theme
+        //                Surface(
+        //                    modifier = Modifier.fillMaxSize(),
+        //                    color = MaterialTheme.colorScheme.background
+        //                ) {
+        //                    Greeting("Android");
+        //                    AddTestBtn(this);
+        //
+        //
+        //                    basicSyntaxTest();
+        //                }
+        //            }
+        //        }
+    }
+
+    private fun initView() {
+        findViewById<Button>(R.id.firstPageBtn)
+            .setOnClickListener {
+                val intent: Intent = Intent(this, FirstActivity::class.java)
+                this.startActivity(intent)
+            }
+
+        findViewById<Button>(R.id.secondPageBtn)
+            .setOnClickListener{
+                val intent: Intent = Intent(this, SecondActivity::class.java)
+                this.startActivity(intent)
+            }
     }
 }
-
-
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(
         modifier = modifier,
-//        color = Color.White,
-    ){
+        //        color = Color.White,
+    ) {
         Text(
             text = "Hello $name!",
         )
@@ -51,11 +70,11 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun AddTestBtn(context : Context){
+fun AddTestBtn(context: Context) {
     Button(onClick = {
         Log.i(TAG, "AddTestBtn: onClick")
-//        Runtime.getRuntime().exit(0);
-        val intent : Intent = Intent(context, FirstActivity::class.java)
+        //        Runtime.getRuntime().exit(0);
+        val intent: Intent = Intent(context, FirstActivity::class.java)
         context.startActivity(intent)
     }) {
         Text(
@@ -66,11 +85,11 @@ fun AddTestBtn(context : Context){
 
 
 fun basicSyntaxTest() {
-    val ks : KotlinBasicSyntax = KotlinBasicSyntax()
-//    ks.doTest()
+    val ks: KotlinBasicSyntax = KotlinBasicSyntax()
+    //    ks.doTest()
 
-    val js : JavaBasicSyntax = JavaBasicSyntax()
-//    js.doTest()
+    val js: JavaBasicSyntax = JavaBasicSyntax()
+    //    js.doTest()
 }
 
 
