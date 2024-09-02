5a81658dbb9232fc4d4c9214634e1a8894165e3e
chenyouhui1 <chenyouhui1@joyy.com>
2024-09-02 15:09:13 +0800

    音、视频

--- Projects/FirstLineAndroidCode/app/src/main/java/com/example/firstlineandroidcode/video/PlayVideoActivity.kt
+++ Projects/FirstLineAndroidCode/app/src/main/java/com/example/firstlineandroidcode/video/PlayVideoActivity.kt
@@ -18,8 +18,14 @@ class PlayVideoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_video)
+
+        /**
+         * VideoView不支持直接播放assets目录下的视频资源，所以我们只能寻找其他的解决方案。
+         * res目录下允许我们再创建一个raw目录，像诸如音频、视频之类的资源文件也可以放在这里，
+         * 并且VideoView是可以直接播放这个目录下的视频资源的。
+         * */
         val uri = Uri.parse("android.resource://$packageName/${R.raw.video}")
-        
+
         val videoView : VideoView = findViewById(R.id.videoView)
         videoView.setVideoURI(uri)
 
