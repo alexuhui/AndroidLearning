9d4282bf21390a34f0949ec3626fe751dd677146
chenyouhui1 <chenyouhui1@joyy.com>
2024-09-05 09:37:22 +0800

    依赖升级

--- Projects/FirstLineAndroidCode/app/build.gradle.kts
+++ Projects/FirstLineAndroidCode/app/build.gradle.kts
@@ -69,17 +69,17 @@ dependencies {
      * core-ktx 库工程名
      * 1.12.0 版本号
      * */
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation ("androidx.work:work-runtime:2.7.1")
+    implementation ("androidx.work:work-runtime:2.9.1")
     implementation ("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation ("com.google.android.material:material:1.1.0")
-    implementation ("de.hdodenhof:circleimageview:3.0.1")
+    implementation ("com.google.android.material:material:1.12.0")
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     /**
      * 至于库依赖声明这里没有用到，它的基本格式是implementation project后面加上要依赖的库的名称，
