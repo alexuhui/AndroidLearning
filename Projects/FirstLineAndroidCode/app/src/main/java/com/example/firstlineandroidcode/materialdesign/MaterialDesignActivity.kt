9598f3904547f41e2831df788a6b2b779ea01bbf
chenyouhui1 <chenyouhui1@joyy.com>
2024-09-05 10:25:08 +0800

    悬浮按钮

--- Projects/FirstLineAndroidCode/app/src/main/java/com/example/firstlineandroidcode/materialdesign/MaterialDesignActivity.kt
+++ Projects/FirstLineAndroidCode/app/src/main/java/com/example/firstlineandroidcode/materialdesign/MaterialDesignActivity.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.firstlineandroidcode.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 
 class MaterialDesignActivity : AppCompatActivity() {
@@ -37,6 +39,11 @@ class MaterialDesignActivity : AppCompatActivity() {
             val  drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
             drawerLayout.closeDrawers()
             true
+        }
+
+        val fab : FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener(){
+            finish()
         }
     }
 
